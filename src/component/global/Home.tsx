ecdc07ec6c55eec718ac3ef9395ac93d02f8bc5f
Shyam Nair <shyam.nair@salesforce.com>
2022-04-24 15:13:34 +0530

    updated README.md file

---
+++ src/component/global/Home.tsx
@@ -0,0 +1,17 @@
+const Home = () => {
+    return (
+        <main>
+            <section>
+                <header>
+                    <h1>Movies on the Tip</h1>
+                </header>
+                <hr />
+                <p>
+                    Movies on the Tip is an online movie manager whose responsibility is to take care of all the activities you can do. 
+                </p>
+            </section>
+        </main>
+    );
+};
+
+export default Home;
